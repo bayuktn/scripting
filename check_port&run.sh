d862a7a131f18f35fffa7adf94153e3378ea74f1
Muhammad Aji Bayu Purbaning katon <44914090+katontsuyoi@users.noreply.github.com>
2021-11-11 11:39:07 +0700

    Yess

--- check_port&run.sh
+++ check_port&run.sh
@@ -7,3 +7,14 @@ do
                 systemctl restart postfix
         fi
 done
+
+##!/bin/bash
+#while :
+#do
+#        sudo lsof -i:8809
+#        if [[ "$?" == 1 ]];
+#        then
+#              cd /home/backend/simotor/log
+#              sudo screen -dmS Rerun_Log_service bash -c './log'
+#        fi
+#done
