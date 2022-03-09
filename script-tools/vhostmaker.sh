acd59eaad07eb9f7cddde77858aac740542f1732
katontsuyoi <katontsuyoi@gmail.com>
2022-03-09 23:21:16 +0700

    penambahan script pembuatan virtual host

--- script-tools/vhostmaker.sh
+++ script-tools/vhostmaker.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+#Detect nginx & certbot Section
+dpkg -l | grep nginx > /dev/null
+if [[ $? != 0 ]]
+then apt update && apt install nginx
+fi
+dpkg -l | grep certbot > /dev/null
+if [[ $? != 0 ]]
+then apt update && apt install cerbot && apt install python3-cerbot-nginx
+fi
 #Input Section
+cip=$(hostname -I | awk '{print $1}')
+echo '<------------------------------------->'
+echo 'This Script credit by IG : @bayuktn'
+echo ' YOUR SERVER IP IS' $cip
+read -n 1 -r -s -p $'[WARNING !!!]\n Please Read This Caution !!\n Configure your DOMAIN FIRST revers to IP address on this Server Before you continue this step...\n Press enter to continue...\n'
 read -p "Write Domain name [ domain.id] > " dom
 read -p "Port Number [8080] > " po
 read -p "Write IP of Services [100.23.42.12] > " ip
+read -p "Write Email for SSL Notif [ex@gmail.com] > " mail 
 #Variable Section
 si_ava='/etc/nginx/sites-available'
 si_ena='/etc/nginx/sites-enabled'
@@ -43,3 +58,14 @@ server {
 	}
 }
 EOF
+ln -s $si_ava/$dom $si_ena/
+nginx -t > .new_log.log
+if [[ $? != 0  ]]
+then 
+	echo "Something Wrong Please Check on new_log.log"
+else
+	systemctl restart nginx
+	certbot  --agree-tos -m $mail  --no-eff-email --redirect -n -q --nginx -d $dom
+fi
+echo 'Congrats you complete to make a VirtualHost revers to this Server'
+echo 'Please check your domain on url '$dom 
