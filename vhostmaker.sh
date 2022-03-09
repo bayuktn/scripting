cbfef3757284858bb26d5d9c6156530e871d8f09
Muhammad Aji Bayu Purbaning katon <katontsuyoi@gmail.com>
2022-03-09 22:01:52 +0700

    git commit -m 'add vhost script'

--- vhostmaker.sh
+++ vhostmaker.sh
@@ -8,5 +8,38 @@ si_ava='/etc/nginx/sites-available'
 si_ena='/etc/nginx/sites-enabled'
 htup='$host'
 host='$http_upgrade'
+reqm='$request_method'
+#Script Section
+cat > $si_ava/$dom << EOF
+server {
+    root /var/www/my-example-domain.com/current/public; # Directly serves anything in the Rails public folder
+    index index.html index.htm index.nginx-debian.html;
+    server_name $dom ; # managed by Certbot
 
-
+    location / {
+        proxy_pass http://$ip:$po; # 8030 is the port the Docker container is running on
+          proxy_set_header Host $htup;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $host;
+        proxy_set_header Connection 'upgrade';
+        proxy_cache_bypass $host;
+        proxy_buffering off;
+        proxy_read_timeout 1800;
+        proxy_hide_header 'access-control-allow-origin';
+        add_header 'access-control-allow-origin' '*' always;
+        add_header 'Access-Control-Allow-Credentials' 'true';
+        add_header 'Access-Control-Allow-Headers' 'Authorization,Accept,Origin,DNT,X-Chisel-Proxied-Url,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range,x-api,*';
+        add_header 'Access-Control-Allow-Methods' 'GET,POST,OPTIONS,PUT,DELETE,PATCH';
+        if ($reqm = 'OPTIONS') {
+          add_header 'Access-Control-Allow-Origin' '*' always;
+          add_header 'Access-Control-Allow-Credentials' 'true';
+          add_header 'Access-Control-Allow-Headers' 'Authorization,Accept,Origin,DNT,X-Chisel-Proxied-Url,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range,x-api,*';
+          add_header 'Access-Control-Allow-Methods' 'GET,POST,OPTIONS,PUT,DELETE,PATCH';
+          add_header 'Access-Control-Max-Age' 1728000;
+          add_header 'Content-Type' 'text/plain charset=UTF-8';
+          add_header 'Content-Length' 0;
+          return 204;
+       		 }
+	}
+}
+EOF
